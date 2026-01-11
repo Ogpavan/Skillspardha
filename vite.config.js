d1d025b99c5d63944afb33b9260fce7250cacc9b
Ogpavan <pawantwp@gmail.com>
2026-01-11 20:11:00 +0530

    kajsdh

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    legacy({
+      targets: ["defaults", "Android >= 8", "Chrome >= 60", "Firefox >= 60"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
   ],
+  build: {
+    target: "es2017",
+  },
 });
